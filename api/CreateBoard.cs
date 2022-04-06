f843bf20385ee10f528eca1006b0e1aeb02e1c7c
anders.stromberg <anders.stroemberg@gmail.com>
2022-04-06 21:54:28 +0200

    Updated model to enable more than one score board

---
+++ api/CreateBoard.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Azure.Data.Tables;
+using System.Linq;
+
+namespace api
+{
+    public static class CreateBoard
+    {
+        [FunctionName("CreateBoard")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "createboard/{boardName:alpha}")] HttpRequest req,
+            string boardName,
+            ILogger log)
+        {
+            try
+            {
+                var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+                var tableClient = new TableClient(connectionString, "Scoreboard");
+
+                var token = Guid.NewGuid().ToString();
+                try
+                {
+                    await tableClient.AddEntityAsync(new ScoreboardTokenEntity
+                    {
+                        PartitionKey = boardName,
+                        Token = token
+                    });
+                    await tableClient.AddEntityAsync(new LowScoreEntity {
+                        PartitionKey = boardName,
+                        RowKey = "LowScore",
+                        Score = -1
+                    });
+                }
+                catch (Exception e)
+                {
+                    return new JsonResult(e);
+                }
+
+                return new JsonResult(new { Name = boardName, Token = token });
+            }
+            catch (Exception e)
+            {
+                log.LogError(e, "Failed to get scoreboard data");
+                return new JsonResult(Enumerable.Empty<ScoreModel>().ToList());
+            }
+        }
+    }
+
+}
